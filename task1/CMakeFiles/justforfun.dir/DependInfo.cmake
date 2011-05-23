047ebdc2ae3ab7ea36af863a19a2f814e658509b
manojalwani <manoj.om.1988@gmail.com>
2011-05-23 07:43:14 +0530

    This is first task; Example HelloVizWorld

---
+++ task1/CMakeFiles/justforfun.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/manoj/Desktop/GSOC Tasks/task1/HelloVizWorld.cxx" "/home/manoj/Desktop/GSOC Tasks/task1/CMakeFiles/justforfun.dir/HelloVizWorld.cxx.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
